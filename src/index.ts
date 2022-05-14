e2d72553c93c1089303775a546f128fbcb00f5f1
xnought <bertuccd@oregonstate.edu>
2022-05-14 13:11:24 -0700

    update example to taxi driver

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,31 @@ import optimalLinearAssignment from "./lap-jv";
 export default optimalLinearAssignment;
 
 // function main() {
+// 	const taxiDriverLocations = [
+// 		[0, 0],
+// 		[1, 1],
+// 	];
+// 	const peopleCallingTaxiLocations = [
+// 		[5, 4],
+// 		[1, 0],
+// 		[1, 1],
+// 		[-1, 1],
+// 	];
+
 // 	const assignments = optimalLinearAssignment({
-// 		assignees: [
-// 			[0, 0, 0],
-// 			[1, 1, 1],
-// 			[5, 5, 2],
-// 		],
-// 		pointsToAssign: [
-// 			[5, 5, 0],
-// 			[1, 0, 0],
-// 			[1, 1, 0],
-// 		],
+// 		assignees: taxiDriverLocations,
+// 		pointsToAssign: peopleCallingTaxiLocations,
 // 	});
-// 	console.log(assignments);
+
+// 	console.log("0 indexing");
+// 	console.log("assignment indices", assignments);
+// 	const taxiDriverAssignment0 = peopleCallingTaxiLocations[assignments[0]];
+// 	const taxiDriverAssignment1 = peopleCallingTaxiLocations[assignments[1]];
+// 	console.log(
+// 		`taxi driver 0 was assigned to person ${assignments[0]} at location (${taxiDriverAssignment0})`
+// 	);
+// 	console.log(
+// 		`taxi driver 1 was assigned to person ${assignments[1]} at location (${taxiDriverAssignment1})`
+// 	);
 // }
 // main();
